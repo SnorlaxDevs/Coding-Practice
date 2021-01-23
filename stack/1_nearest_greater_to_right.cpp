6d600c026a1c11af1170b13790ab63a93d242a9a
Fahad Khan <fahadkgla@gmail.com>
2021-01-24 04:04:25 +0530

    Stack Basic Questions

---
+++ stack/1_nearest_greater_to_right.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> ngr(vector<int> nums){
+    vector<int> out;
+    stack<int> s;
+    int n = nums.size();
+    for(int i = n-1; i >= 0; i--){
+        if(s.empty()){
+            out.push_back(-1);
+        }
+        else if(s.top() > nums[i]){
+            out.push_back(s.top());
+        }
+        else{
+            while(s.size() > 0 && s.top() <= nums[i]){
+                s.pop();
+            }
+            if(s.size() == 0){
+                out.push_back(-1);
+            }else{
+                out.push_back(s.top());
+            }
+        }
+        s.push(nums[i]);
+    }
+    reverse(out.begin(), out.end());
+    return out;
+}
+
+int main(){
+    vector<int> nums = {1,2,4,3,5,2,8};
+    vector<int> out = ngr(nums);
+    for(auto i: out){
+        cout << i << ", ";
+    }
+    return 0;
+}
